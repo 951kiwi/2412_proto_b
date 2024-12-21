9543235b483b89317d03b95cc504911cde8543aa
Shigema <tsubakyun4869@gmail.com>
2024-12-21 13:23:14 +0900

    [update] タイトルシーンの演出、トランジションのフェード [add] 仮のタイトルロゴ

---
+++ Assets/Scripts/OutGame/Fade.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OutGame
+{
+    public class Fade : MonoBehaviour
+    {
+        [SerializeField, Header("パネル")] private Image _panel;
+        [SerializeField, Header("かかる時間")] private float _duration = 0.5f;
+
+        [SerializeField, Header("シーン開始時にFadeOutする")]
+        private bool _flag;
+
+        private void Start()
+        {
+            if (_flag) FadeOut();
+        }
+
+        private void Update()
+        {
+            // テスト
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                FadeIn();
+            }
+            else if (Input.GetKeyDown(KeyCode.J))
+            {
+                FadeOut();
+            }
+        }
+
+        /// <summary>
+        /// FadeIn FadeOut
+        /// </summary>
+        private IEnumerator FadeImage(float startAlpha, float endAlpha)
+        {
+            var color = _panel.color;
+            var time = 0f;
+            while (time < _duration)
+            {
+                time += Time.deltaTime;
+                var alpha = Mathf.Lerp(startAlpha, endAlpha, time / _duration);
+                _panel.color = new Color(color.r, color.g, color.b, alpha);
+                yield return null;
+            }
+        }
+
+        /// <summary>
+        /// 暗くなる
+        /// </summary>
+        public void FadeIn()
+        {
+            StartCoroutine(FadeImage(0, 1));
+        }
+
+        /// <summary>
+        /// 明るくなる
+        /// </summary>
+        public void FadeOut()
+        {
+            StartCoroutine(FadeImage(1, 0));
+        }
+    }
+}
