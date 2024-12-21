585cccead71702246da30e3e2052209bd75cc1e6
951kiwi <aimu.shibahara@gmail.com>
2024-12-21 18:24:11 +0900

    敵MOB 挙動追加

--- Assets/C#/mob.cs
+++ Assets/C#/mob.cs
@@ -7,8 +7,10 @@ public class mob : MonoBehaviour
     [SerializeField] private float jump = 5f;
     [SerializeField] private float speed = 1f;
     [Header("接触判定")] public EnemyCollisionCheck checkCollision;
+    [Header("接触判定あたま")] public EnemyCollisionCheck HeadcheckCollision;
     [Header("画面外でも行動する")] public bool nonVisibleAct = false;
     [SerializeField] bool rightTleftF = false;
+    bool isdead = false;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -19,29 +21,42 @@ public class mob : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isdead)
+        {
+            
+                transform.Rotate(new Vector3(0, 0, 5));
+            
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (GameObject.Find("G_Collider") == collision.gameObject){
-            Destroy(this.gameObject);
-        }   
-        if (checkCollision.isOn)
+    {  
+        if (checkCollision.isWall)
         {
             rightTleftF = !rightTleftF;
         }
+        if (HeadcheckCollision.isHead){
+            isdead = true;
+            this.GetComponent<Collider2D>().enabled = false;
+            rb.velocity = new Vector2(2f, 4f);
+            Destroy(rb , 5f);
+        }
 
-        if (rightTleftF)
+        if (!isdead)
         {
-            Vector2 vector2 = new Vector2(speed, jump);
-            rb.velocity = vector2;
+            if (rightTleftF)
+            {
+                Vector2 vector2 = new Vector2(speed, jump);
+                rb.velocity = vector2;
+            }
+            else
+            {
+                Vector2 vector2 = new Vector2(-speed, jump);
+                rb.velocity = vector2;
+            }
         }
-        else
-        {
-            Vector2 vector2 = new Vector2(-speed, jump);
-            rb.velocity = vector2;
-        }
+        
+
 
     }
 
