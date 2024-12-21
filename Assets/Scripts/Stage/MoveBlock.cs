71d741747ba23ea8acf7a5bbc2f1070b04fe697b
koumei-takamori <koumeidesuyo1302@gmail.com>
2024-12-21 11:00:17 +0900

    MoveBlock追加 ＋ MoveGroundTag追加 + ステージ1シーン(仮)追加

---
+++ Assets/Scripts/Stage/MoveBlock.cs
@@ -0,0 +1,37 @@
+/**********************************************************
+ *
+ *  MoveBlock.cs
+ *  ìÆÇ≠ÉuÉçÉbÉN
+ *
+ *  êßçÏé“ : ˚¸êX ‡äñæ
+ *  êßçÏì˙ : 2024/12/20
+ *
+ *********************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : MonoBehaviour
+{
+    [SerializeField]
+    private int m_speed;
+    [SerializeField]
+    private Vector2 m_startPos;
+    [SerializeField]
+    private float m_length;
+    [SerializeField]
+    private Rigidbody2D m_rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float posX = m_startPos.x + Mathf.PingPong(Time.time * m_speed, m_length);
+        m_rigidBody.MovePosition(new Vector3(posX, m_startPos.y));
+    }
+}
