0ed54423a5464ac89df9e8359791b1257fe2303f
951kiwi <aimu.shibahara@gmail.com>
2024-12-21 09:58:38 +0900

    背景色とplayerを同期

---
+++ Assets/C#/mob.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mob : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    float beforePosY = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(beforePosY == this.transform.position.y){
+            Vector2 vector2= new Vector2(-1f,5f);
+            rb.AddForce(vector2,ForceMode2D.Impulse);
+        }
+        else{
+            beforePosY = this.transform.position.y;
+        }
+    }
+}
